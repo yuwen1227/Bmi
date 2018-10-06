5cf455e84e36d290a648494c29d307b644726edd
yuwen1227 <coco4522@gmail.com>
2018-10-06 22:52:19 +0800

    find view by id

--- app/src/main/java/com/wen/bmi/MainActivity.java
+++ app/src/main/java/com/wen/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.wen.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void bmi (View view){
+        Log.d("MainActivity","bmi");
+        EditText edWeight = findViewById(R.id.ed_weight);
+        EditText edHeight = findViewById(R.id.ed_height);
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        Log.d("MainActivity",w + "/" + h);
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("MainActivity","bmi"+" ");
+
+
     }
 }
